bb13a26e5dd8b457c84771c8164351747a8d19d2
FabioKido <fabiohenryquemesquita@gmail.com>
2020-03-26 17:49:29 -0300

    Feat: Configuration Page Created

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import "./styles.css";
 
 const Header = () => (
 	<header id="main-header">
 		<div className="links">
-			<a href="/">Em Andamento</a>
-			<a href="/">Pedidos Realizados</a>
-			<a href="/">Todos os Pedidos</a>
-			<a href="/">Configurações</a>
+			<Link to="/">Pedidos</Link>
+			<Link to="/">Registros</Link>
+			<Link to="/config">Configurações</Link>
 		</div>
 	</header>
 );
 
 export default Header;
+
+// Em Andamento
+// Pedidos Realizados
+// Todos os Pedidos
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { isAuthenticated } from './services/auth';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
+import Configuration from './pages/Configuration';
 import Main from './pages/main';
 import Product from "./pages/product";
 import Register from "./pages/register";
@@ -26,8 +27,7 @@ const Routes = () => (
 			<Switch>
 				<Route exact path="/" component={Main} />
 				<Route path="/login" component={SignIn} />
-				//<Route path="/signup" component={Register} />
-				//<PrivateRoute path="/product/:id" component={Product} />
+				<PrivateRoute path="/config" component={Configuration} />
 				<Route path="*" component={() => <h1>Page not found</h1>} />
 			</Switch>
 		</Fragment>	
@@ -35,3 +35,8 @@ const Routes = () => (
 );
 
 export default Routes;
+
+/*
+<Route path="/signup" component={Register} />
+<PrivateRoute path="/product/:id" component={Product} />
+*/
